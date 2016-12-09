d15280f1f95212d3c94e016a4e273c6fa6056b9f
Luca Hagel <hagel.luca@gmail.com>
2016-12-09 15:28:24 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209001917) do
+ActiveRecord::Schema.define(version: 20161209232257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,10 @@ ActiveRecord::Schema.define(version: 20161209001917) do
 
   create_table "teams", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "user_id"
-    t.boolean  "is_public"
+    t.boolean  "is_public",  default: false
     t.index ["user_id"], name: "index_teams_on_user_id", using: :btree
   end
 
