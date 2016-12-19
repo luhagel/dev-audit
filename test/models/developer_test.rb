9f6772d0e56f4db7e7cc452d180e94875f3a1af6
Luca Hagel <hagel.luca@gmail.com>
2016-12-19 16:34:41 +0100

    developer model tests

--- test/models/developer_test.rb
+++ test/models/developer_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class DeveloperTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @test_user = User(:one)
+    @test_team = Team.create(name: "Test Team", user: @test_user)
+  end
+  test "Only allows valid developers" do
+    valid_developer = Developer.create(name: "testuser", username: "octocat", team: @test_team)
+    invalid_developer = Developer.create(name: "testuser2")
+
+    assert valid_developer.isValid?
+    assert !invalid_developer.isValid?
+  end
 end
