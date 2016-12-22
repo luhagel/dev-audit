b2c7f9d81d429a0170bd6142be78e7cc1a94624c
Luca Hagel <hagel.luca@gmail.com>
2016-12-22 09:48:23 +0100

    fix fixtures realted test fails

--- test/models/developer_test.rb
+++ test/models/developer_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class DeveloperTest < ActiveSupport::TestCase
   def setup
-    @test_user = User(:one)
+    @test_user = users(:one)
     @test_team = Team.create(name: "Test Team", user: @test_user)
   end
   test "Only allows valid developers" do
     valid_developer = Developer.create(name: "testuser", username: "octocat", team: @test_team)
     invalid_developer = Developer.create(name: "testuser2")
 
-    assert valid_developer.isValid?
-    assert !invalid_developer.isValid?
+    assert valid_developer.valid?
+    assert !invalid_developer.valid?
   end
 end
