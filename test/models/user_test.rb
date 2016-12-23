dfaf4b892432740ddd911349502e0bbf319b471d
Luca Hagel <hagel.luca@gmail.com>
2016-12-23 16:17:41 +0100

    fix testuser

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup 
-    @user = users(:one)
+    @user = User.new(name: "testuser", email: "testuser@example.com", password: "testpw", password_confirmation: "testpw")
   end
 
   test "should be valid" do
