1f1e188755c9cd6370786413a13cf2d7e92e9090
Luca Hagel <hagel.luca@gmail.com>
2017-01-07 04:45:14 +0100

    fix graph length, scope errors in sidenav

--- app/helpers/developers_helper.rb
+++ app/helpers/developers_helper.rb
@@ -13,8 +13,8 @@ module DevelopersHelper
   end
 
   def generate_graph_svg(graph_array)
-    svg = '<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 360 60">'
-    svg += '<line x1="0" x2="360" y1="30" y2="30" stroke-width="1" stroke="black"/>'
+    svg = '<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 ' + String(graph_array.count) + ' 60">'
+    svg += '<line x1="0" x2="' + String(graph_array.count) + '" y1="30" y2="30" stroke-width="1" stroke="black"/>'
     line_offset = 0
     graph_array.each do |line|
       svg += '<line x1="' + String(line_offset) + '" x2="' + String(line_offset) + '" y1="' + String(30 - line.to_i * 1.5 ) + '" y2="' + String(30 + line.to_i * 1.5) + '" stroke-width="1"  class="graph-line" />'
