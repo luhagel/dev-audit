42f118fc3045cc5188329a85f9229f0e57e01cf0
Luca Hagel <hagel.luca@gmail.com>
2017-01-08 21:08:57 +0100

    Add Pages Feature test, remove integration tests

---
+++ test/features/can_access_home_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class CanAccessHomeTest < Capybara::Rails::TestCase
+  scenario "has content" do
+    visit root_path
+    page.must_have_content "Welcome to DevAudit!"
+  end
+end
