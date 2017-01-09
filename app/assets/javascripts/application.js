5eba4af43f00d01457a6939363016fa3f0257cf7
Luca Hagel <hagel.luca@gmail.com>
2017-01-09 06:03:01 +0100

    Add mobile nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+document.getElementById("nav-toggle").addEventListener ("click", toggleNav);
+function toggleNav() {
+        var nav = document.getElementById("nav-menu");
+        var className = nav.getAttribute("class");
+        if(className == "nav-right nav-menu") {
+            nav.className = "nav-right nav-menu is-active";
+        } else {
+            nav.className = "nav-right nav-menu";
+        }
+}
