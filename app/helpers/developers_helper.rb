c2fe2a9ead2714e7df1639d1fdab62462d08999e
Luca Hagel <hagel.luca@gmail.com>
2017-01-12 02:56:15 +0100

    Show Last updated, add missing N/As

--- app/helpers/developers_helper.rb
+++ app/helpers/developers_helper.rb
@@ -1,18 +1,20 @@
 module DevelopersHelper
   include ActionView::Helpers::OutputSafetyHelper
 
+  include GithubUsersHelper
+
   def update_all_developer_info
     @developers = Developer.all
     save_counter = 0
     @developers.each do |dev|
-      dev.github_user.contrib_data = get_contrib_graph(dev.username)
+      dev.github_user.contrib_data = get_contrib_data(dev.username)
       save_counter += 1 if dev.save
     end
     puts 'SUCCESS: Updated ' + String(save_counter) + '/' + String(@developers.count) + ' Developers!'
   end
 
   def update_developer_info(dev)
-    dev.github_user.contrib_data = get_contrib_graph(dev.username)
+    dev.github_user.contrib_data = get_contrib_data(dev.username)
   end
 
   def owner?(team)
