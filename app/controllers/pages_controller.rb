7de7bf822f1d1e1db6fe3c99fff4ff3e53d5d94e
Luca Hagel <hagel.luca@gmail.com>
2017-01-18 14:11:10 -0800

    add paimages, hide letsencrypt id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,13 @@ class PagesController < ApplicationController
       render text: "Ki_x17PcnAoLB_B2S49bkvnsc4Zs70XX9pPxtwL_Hzo.oRrCKQrTcxmGONE248b4unEzXiFcjwPfQUl4bmFyQEU"
     end
 
+    def paimages
+      @links = []
+      Team.find(1).developers.each do |dev|
+        @links += ["https://avatars1.githubusercontent.com/u/#{dev.github_user.user_id}?v=3&s=128"]
+      end
+    end
+
     private
     def valid_page?
       File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
