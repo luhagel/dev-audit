8f0c553c56fe1151bda6dafad27c5c8342243d21
Luca Hagel <hagel.luca@gmail.com>
2017-01-19 01:27:04 -0800

    add username crawler, placehiolder add boxes

--- app/helpers/developers_helper.rb
+++ app/helpers/developers_helper.rb
@@ -4,4 +4,30 @@ module DevelopersHelper
   def owner?(team)
     current_user == team.user && !current_user.nil?
   end
+
+  def find_account_matches
+    @developers = Developer.all
+    @developers.each do |dev|
+      if exists?('https://twitter.com/' + dev.username) && dev.twitter_username == ""
+        dev.twitter_username = dev.username
+      end
+      if exists?('https://medium.com/@' + dev.username) && dev.medium_username == ""
+        dev.medium_username = dev.username
+      end
+      dev.save
+    end
+  end
+
+  def exists?(url)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    response = http.request(request)
+    puts response.code.to_s + ': ' + url
+    response.code.to_i == 200
+  end
 end
