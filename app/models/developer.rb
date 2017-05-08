59443a6eb44d032986120ce5be003985448679b2
Luca Hagel <hagel.luca@gmail.com>
2017-05-07 22:46:53 -0700

    implement search by name/github login

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -5,5 +5,9 @@ class Developer < ApplicationRecord
   has_one :github_user, dependent: :destroy
 
   validates :username, presence: true
+
+  def self.search(search)
+    joins(:github_user).where("github_users.name ILIKE ? OR github_users.login ILIKE ?", "%#{search}%", "%#{search}%").references(:github_user)
+  end
 end
 
